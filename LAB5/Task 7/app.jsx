8e95daf368ddbdbf71e7a49b7c96215c356e957b
D1m0haa <bertinoks1307@gmail.com>
2024-06-06 13:19:26 +0300

    LAB 5

---
+++ LAB5/Task 7/app.jsx
@@ -0,0 +1,6 @@
+// WHAT: This function returns a string that will be rendered
+const PrintHello = () => {
+    return <h1>I love React</h1>;
+}
+                 //what       //where
+ReactDOM.render(PrintHello(), myDiv);
